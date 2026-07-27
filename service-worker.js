25e629805cd2a00bd3b7cce8125aa70d9b09a2f7
genesisgns91 <genesisgns@gmail.com>
2026-07-27 15:30:11 -0300

    Update icon paths in APP_SHELL array

--- service-worker.js
+++ service-worker.js
@@ -28,10 +28,10 @@ const APP_SHELL = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/icon-maskable-192.png',
-  '/icons/icon-maskable-512.png'
+  'icon-192.png',
+  'icon-512.png',
+  'icon-maskable-192.png',
+  'icon-maskable-512.png'
 ];
  
 // ── INSTALL ────────────────────────────────────────────────────────────
